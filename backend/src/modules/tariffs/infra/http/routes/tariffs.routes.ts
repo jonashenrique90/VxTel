6f2e4614914644def753d874d667564bd72ee3e5
jonashenrique90 <jonas.herique90@gmail.com>
2020-12-18 03:20:39 -0300

    remove front-end do projeto.

--- backend/src/modules/tariffs/infra/http/routes/tariffs.routes.ts
+++ backend/src/modules/tariffs/infra/http/routes/tariffs.routes.ts
@@ -4,14 +4,6 @@ import TariffsController from '../controllers/TariffsController';
 const tariffsRouter = Router();
 const tariffsController = new TariffsController();
 
-
-// tariffsRouter.get('/', async (request, response) => {
-    
-//     const tariffs = await tariffsRepository.find();
-//     return response.json(tariffs);
-// });
-
-
 tariffsRouter.post('/', tariffsController.create);
 
 export default tariffsRouter;
